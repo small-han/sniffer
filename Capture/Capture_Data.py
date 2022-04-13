4cf515c7c0fa7e900532e2edc96de18e02811ad2
han <13378766179@163.com>
2022-04-13 15:41:13 +0800

    add listview

--- Capture/Capture_Data.py
+++ Capture/Capture_Data.py
@@ -1,33 +1,41 @@
 from scapy.all import *
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout
-import threading
+from designer.designer import Ui_Form
+from PyQt5.QtCore import QStringListModel
 
-
-class MainWidget(QWidget):
+class MainWidget(QWidget,Ui_Form):
 
     def __init__(self):
         super(MainWidget, self).__init__()
-        self.resize(1000, 500)
+        self.setupUi(self)
         self.sniffer = None
+        self.packets=[]
 
+        self.start_button.pressed.connect(self.capture)
+        self.stop_button.pressed.connect(self.stop)
+    """
         self.start_button = QPushButton("开始嗅探", self)
         self.stop_button = QPushButton("停止嗅探", self)
         self.button_layout = QHBoxLayout()
         self.init_button()
 
         self.setLayout(self.button_layout)
-
     def init_button(self):
         self.button_layout.addWidget(self.start_button)
         self.button_layout.addWidget(self.stop_button)
         self.start_button.pressed.connect(self.capture)
         self.stop_button.pressed.connect(self.stop)
+    """
 
     def set_sniffer(self, count, filter):
         self.sniffer = AsyncSniffer(count=count, filter=filter, prn=self.data_handle)
 
     def data_handle(self, packet):
-        print(packet.summary())
+        self.packets.append(packet)
+        show_list=[i.summary() for i in self.packets]
+        slm=QStringListModel()
+        slm.setStringList(show_list)
+        self.listView.setModel(slm)
 
     def capture(self):
         self.sniffer.start()
